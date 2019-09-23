832a1d2569fcf570f578c75ee79e38095946afe2
partythenwork <55715618+partythenwork@users.noreply.github.com>
2019-09-23 15:36:58 -0500

    Update 2darray.py

--- 2darray.py
+++ 2darray.py
@@ -4,6 +4,11 @@ import pprint
 WIDTH = 10
 HEIGHT = 5
 
+#this is a study to populate a 2d array and assign random values into the array.
+#the values are going to be from 0 to 20, but if the value is less than 10 then we 
+#add a 0 in front to make sure that there are two digits, but the values will have
+#to be stored as characters instead of an integer
+
 #create the 2d array and assign all values to integer 0
 map = [[0 for i in range(WIDTH)] for j in range(HEIGHT)]
 #print out the 2d array to verify contents
